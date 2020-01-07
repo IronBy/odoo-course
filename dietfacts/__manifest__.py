23890281dfc947796915b4fa4f0ae3384b598b4c
Valery Sushko <ironenby@gmail.com>
2020-01-07 13:41:20 +0000

    Initial commit

---
+++ dietfacts/__manifest__.py
@@ -0,0 +1,9 @@
+{
+  'name': "Dietfacts",
+  'version': "1.0",
+  'author': "Greg Moss, OdooClass.com",
+  'description': "Adds nutrition information to products and some other stuff",
+  'depends': ['sale'],
+  'data': ["dietfacts_view.xml"],
+  'installable': True
+}
---
+++ dietfacts/dietfacts.py
@@ -0,0 +1,12 @@
+# Dietfacts application
+from odoo import models, fields
+
+# Extend product.template model with calories
+class Dietfacts_product_template(models.Model):
+   _name = 'product.template'
+   _inherit = 'product.template'
+
+   calories = fields.Integer("Calories") 
+   servingsize = fields.Float("Serving Size")
+   lastupdated = fields.Date("Last Updated")
+
