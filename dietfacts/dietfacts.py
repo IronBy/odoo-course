8d3aa0e130a424a3ea95a771410365bd417b38a4
Sushko, Valery <v.sushko@itransition.com>
2020-01-08 18:30:31 +0300

    Use two spaces instead of three

--- dietfacts/dietfacts.py
+++ dietfacts/dietfacts.py
@@ -3,11 +3,11 @@ from odoo import models, fields
 
 # Extend product.template model with calories
 class Dietfacts_product_template(models.Model):
-   _name = 'product.template'
-   _inherit = 'product.template'
+  _name = 'product.template'
+  _inherit = 'product.template'
 
-   calories = fields.Integer("Calories") 
-   servingsize = fields.Float("Serving Size")
-   lastupdated = fields.Date("Last Updated")
-   dietitem = fields.Boolean("Diet Item")
+  calories = fields.Integer("Calories")
+  servingsize = fields.Float("Serving Size")
+  lastupdated = fields.Date("Last Updated")
+  dietitem = fields.Boolean("Diet Item")
 
