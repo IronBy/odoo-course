1e07ea6debef687aba59beb810cedcf0c320c271
Sushko, Valery <v.sushko@itransition.com>
2020-01-09 19:10:54 +0300

    Add Meal items model

--- dietfacts/dietfacts.py
+++ dietfacts/dietfacts.py
@@ -9,3 +9,11 @@ class Dietfacts_product_template(models.Model):
   calories = fields.Integer("Calories")
   servingsize = fields.Float("Serving Size")
   lastupdated = fields.Date("Last Updated")
+
+class Dietfacts_res_users_meal(models.Model):
+  _name = 'res.users.meal'
+  name = fields.Char("Meal Name")
+  meal_date = fields.Datetime("Meal Date")
+  # item_ids = fields.One2many()
+  user_id = felds.Many2one("res.users", "Meal User")
+  notes = fields.Text("Meal Notes")
